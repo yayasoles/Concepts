9dfd65858d50e4480101ec94f13ef5041452bf11
yayasoles@gmail.com <yayasoles@gmail.com>
2022-07-08 19:02:47 -0700

    commiting the core library path and util

---
+++ Files/Core.js
@@ -0,0 +1,7 @@
+const path=require("path");
+const util=require("util");
+const v8=require("v8");
+console.log(path.basename(__filename));
+const dirUploads=path.join(__dirname,"www","files","uploads");
+console.log(dirUploads);
+util.log(v8.getHeapStatistics());
