7b4f6ce4bba960bcb8bd925a0fab9ce8223fdcff
yayasoles@gmail.com <yayasoles@gmail.com>
2022-07-09 14:19:27 +0300

    the Event module included and the eventEmitter constructor is inherited by myown object and I can emit customEvent

--- Files/Process.js
+++ Files/Process.js
@@ -1,24 +1,23 @@
-let quesons=["what is your name?","what is your hobby", "what is your prefered Programing languge"]
-let answers=[];
-const ask=(index)=>{
+
+let quesons = ["what is your name?", "what is your hobby", "what is your prefered Programing languge"]
+let answers = [];
+const ask = (index) => {
     process.stdout.write(`\n\n\n\n ${quesons[index]}`);
     process.stdout.write("  >  ");
 }
-process.stdin.on('data',(data)=>{
+process.stdin.on('data', (data) => {
     // process.stdout.write(`\n ${data.toString().trim()} \n`);
     answers.push(data.toString().trim());
-    if(answers.length<quesons.length){
+    if (answers.length < quesons.length) {
         ask(answers.length);
-    }else{
+    } else {
         process.exit();
     }
-})
-process.on('beforeExit',()=>{
-    process.stdout.write("you are exiting");
 });
-process.on('exit',()=>{
-    process.stdout.write("\n\n\n\n");
-process.stdout.write(` go ${answers[1]}  ${answers[0]}  you can finish writing ${answers[2]}`);
-    process.stdout.write("\n\n\n\n");
-})
 ask(0);
+
+
+process.on('beforeExit', () => {
+    console.log("code");
+});
+
